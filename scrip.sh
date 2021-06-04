5baa9f9dd1fcfe1c2aba09881870f9442beffb1f
andriylew <38072028+andriylew@users.noreply.github.com>
2021-06-04 11:28:47 +0300

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
-yum install sbt
+Curl https://bintray.com/sbt/rpm/rpm > Bintray-sbt-rpm.repo
+sudo mv bintray-sbt-rpm.repo/etc/yum.repos.d/
+sudo yum install SBT
